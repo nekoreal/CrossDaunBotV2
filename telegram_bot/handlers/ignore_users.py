1e7dfb087afa9bcb28d70dd198332f1c20e82219
nekoreal <amakoev8938@gmail.com>
2026-01-27 22:48:14 +0300

    микрофикс ignore

--- telegram_bot/handlers/ignore_users.py
+++ telegram_bot/handlers/ignore_users.py
@@ -3,16 +3,17 @@ from telebot.types import Message
 from telegram_bot.bot import bot
 
 IGNORE_USERS = [
-    862249650
+    862249650, 
 ]
 
-def check_ignore_list(message:Message):
+def check_ignore_list(message:Message): 
     if message.reply_to_message:
         return message.reply_to_message.from_user.id in IGNORE_USERS or message.from_user.id in IGNORE_USERS
     return message.from_user.id in IGNORE_USERS
 
 @bot.message_handler(
-    func=lambda message: check_ignore_list(message)
+    func=lambda message: check_ignore_list(message),
+    content_types=['text','photo','video','sticker','document','audio','voice','video_note','location','contact', 'animation', "poll"],
 )
-def ignore_users(message:Message):
+def ignore_users(message:Message):  
     pass
