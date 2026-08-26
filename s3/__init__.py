42625e8df2d95050369b145797a4fa6660dec64e
NekoReal <amakoev8938@gmail.com>
2026-08-26 03:21:47 +0300

    photo db model and controller

---
+++ s3/__init__.py
@@ -0,0 +1,10 @@
+from client import S3Client
+import os
+
+
+s3_client = S3Client(
+    endpoint_url=os.getenv("S3_ENDPOINT_URL"),
+    access_key=os.getenv("MINIO_ROOT_USER"),
+    secret_key=os.getenv("MINIO_ROOT_PASSWORD"),
+    bucket_name=os.getenv("S3_BUCKET_NAME")
+)
---
+++ telegram_bot/tg_db/models/photo.py
@@ -0,0 +1,44 @@
+
+from sqlalchemy import Column, ForeignKey, Integer, String, BigInteger, Boolean
+from telegram_bot.tg_db import Base
+from sqlalchemy.orm import relationship
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True, nullable=False, autoincrement=True)
+    name = Column(String(100), unique=True, nullable=False, collation="utf8mb4_bin") 
+
+    photo = relationship("Photo", back_populates="category" )
+
+    def to_dict(self):
+        return { 
+            "id": self.id,
+            "name": self.name
+        }
+
+    def __repr__(self):
+        return f"<Category(id={self.id}, name={self.name})>"
+
+
+class Photo(Base):
+    __tablename__ = "photos"
+
+    id = Column(Integer, primary_key=True, index=True, unique=True, nullable=False, autoincrement=True)
+    tg_id = Column(BigInteger, unique=False, nullable=False )
+    file_path = Column(String(255), unique=True, nullable=False, collation="utf8mb4_bin")
+    category_id = Column(Integer,  foreign_key=ForeignKey("categories.id", ondelete="SET NULL"), index=True)
+
+    category = relationship("Category", back_populates="photo")
+
+    def to_dict(self):
+        return {  
+            "id": self.id,
+            "tg_id": self.tg_id,
+            "file_path": self.file_path,
+            "category_id": self.category_id,
+            "category": self.category.to_dict() if self.category else None,
+        }
+
+    def __repr__(self):
+        return f"<Photo(tg_id={self.tg_id}, file_path={self.file_path}, category_id={self.category_id})>"
