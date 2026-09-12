03c1a4ae2d5d075a20132058386dbb9f0fe71d4d
NekoReal <amakoev8938@gmail.com>
2026-09-12 16:08:14 +0300

    fix: open for al gallery

--- flask_server/gallery.py
+++ flask_server/gallery.py
@@ -14,12 +14,21 @@ gallery_bp = Blueprint('gallery_bp', __name__)
   
 
 @gallery_bp.route("/gallery/get_categories")
-def get_categories():
-    user = session.get("user")
-    if not user:
-        return "Ошибка авторизации", 400
+def get_categories():  
+    return get_all_categories_dict(), 200 
 
-    return get_all_categories_dict(), 200
+@gallery_bp.route("/gallery/paginate_photos")
+def paginate_photos():  
+    category_id = request.args.get('category', type=str)
+    page = request.args.get('page', default=1, type=int)
+    limit = request.args.get('limit', default=32, type=int) 
+
+    return photo_urls_paginate(category_id=category_id, page=page, limit=limit), 200
+
+@gallery_bp.route("/gallery")
+def gallery_page(): 
+    user = session.get("user") or None
+    return render_template("gallery.html", user=user)
 
 
 @gallery_bp.route("/gallery/resort")
@@ -32,26 +41,6 @@ def resort_photo():
     if move_photo_to_category(photo_id=photo_id, category_name=None):
         return {"message": "Отправлено на пересортировку"}, 200
     return {"message": "Ошибка"}, 400
- 
-
-@gallery_bp.route("/gallery/paginate_photos")
-def paginate_photos():
-    user = session.get("user")
-    if not user:
-        return "Ошибка авторизации", 400
-
-    category_id = request.args.get('category', type=str)
-    page = request.args.get('page', default=1, type=int)
-    limit = request.args.get('limit', default=32, type=int)
-    
-
-    return photo_urls_paginate(category_id=category_id, page=page, limit=limit), 200
-
-@gallery_bp.route("/gallery")
-def gallery_page(): 
-    user = session.get("user") or None
-    return render_template("gallery.html", user=user)
-
 
 
 
